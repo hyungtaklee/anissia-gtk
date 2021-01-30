dbe84c83879ea9002e21985d689495cf204882b2
bora-dong <28820367+bora-dong@users.noreply.github.com>
2021-01-30 20:30:11 +0900

    fix typo on include guard

--- anissiaappwin.h
+++ anissiaappwin.h
@@ -1,5 +1,5 @@
 #ifndef __ANISSIAPPWIN_H
-#define __ANISSIAAPWIN_H
+#define __ANISSIAPPWIN_H
 
 #include <gtk/gtk.h>
 #include "anissiaapp.h"
@@ -7,7 +7,7 @@
 #include "cJSON.h"
 
 #define ANISSIA_APP_WINDOW_TYPE (anissia_app_window_get_type())
-G_DECLARE_FINAL_TYPE(AnissiaAppWindow, anissia_app_window, ANISSIA, APP_WINDOW, GtkApplicationWindow);
+G_DECLARE_FINAL_TYPE (AnissiaAppWindow, anissia_app_window, ANISSIA, APP_WINDOW, GtkApplicationWindow);
 
 AnissiaAppWindow *anissia_app_window_new(AnissiaApp *app);
 void anissia_app_window_open(AnissiaAppWindow *win, cJSON *json, const gchar *basename);
